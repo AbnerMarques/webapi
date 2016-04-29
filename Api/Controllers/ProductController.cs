d7d5ddec5f2312d8912f285f30e12c5c37909c02
AbnerMarques <abner.marques01@fatec.sp.gov.br>
2016-04-29 09:45:46 -0300

    adição do get parametririzado

--- Api/Controllers/ProductController.cs
+++ Api/Controllers/ProductController.cs
@@ -38,9 +38,29 @@ namespace Api.Controllers
         }
 
         // GET: api/Product/5
-        public string Get(int id)
+        public IHttpActionResult Get(int id)
         {
-            return "value";
+            var produto1 = new Product();
+            produto1.Id = 1;
+            produto1.Name = "Placa de Rede";
+            produto1.Category = "Hardware";
+            produto1.Price = 120m;
+            listaDeProdutos.Add(produto1);
+
+            var produto2 = new Product();
+            produto2.Id = 2;
+            produto2.Name = "Memória DDR4";
+            produto2.Category = "Hardware";
+            produto2.Price = 80m;
+            listaDeProdutos.Add(produto2);
+
+            //Forma mais simples de um novo objeto 'Product'.
+            var produto3 = new Product { Id = 3, Name = "Placa Mãe", Category = "Hardware", Price = 480m };
+            listaDeProdutos.Add(produto3);
+
+            var produto = listaDeProdutos.Find(x => x.Id == id);
+
+            return Ok(produto);
         }
 
         // POST: api/Product
